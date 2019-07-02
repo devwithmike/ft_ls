ed16fec124c7365b2500be3e4da815aabdda0f80
Michael MEYER <mimeyer@c4r21s3.wethinkcode.co.za>
2019-07-02 11:00:32 +0200

    hsgdhdsg

--- ft_ls.c
+++ ft_ls.c
@@ -6,45 +6,14 @@
 /*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 08:53:30 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/07/01 16:23:06 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/07/02 11:00:13 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "includes/ft_ls.h"
 
-unsigned char		ft_flags(int ac, char **av)
-{
-	int				i;
-	char			**str;
-	unsigned char	flags;
-
-	flags = 0;
-	i = 1;
-	while (i < ac)
-	{
-		str = &av[i];
-		while (**str == 'l' || **str == 'a' || **str == 'R' ||
-			**str == 'r' || **str == 't' || **str == ' ' || **str == '-')
-		{
-			if (**str == 'l')
-				flags |= 1;
-			if (**str == 'a')
-				flags |= 2;
-			if (**str == 'R')
-				flags |= 4;
-			if (**str == 'r')
-				flags |= 8;
-			if (**str == 't')
-				flags |= 16;
-			(*str)++;
-		}
-		i++;
-	}
-	return (flags);
-}
-
-int main(int ac, char **av)
+void basic_print(int ac, char **av)
 {
 	unsigned char flags;
 	struct dirent *de;
@@ -58,13 +27,13 @@ int main(int ac, char **av)
         return 0;
     }
     while ((de = readdir(dr)) != NULL)
-    	if (ac == 1)
+    	if (!flags)
     	{
     		if (de->d_name[0] != '.')
         		printf("%s\t", de->d_name);
         }
     	else
-    		if	((flags & 2) && (!(flags & 1) && (!(flags & 4) 
+    		if	((flags & 2) && (!(flags & 1) && (!(flags & 4)
     			&& (!(flags & 8)) && (!(flags & 16)))))
     			printf("%s\t", de->d_name);
 	if (flags & 1)
@@ -77,5 +46,44 @@ int main(int ac, char **av)
     	printf("The flag t is there... but I ain't handeling that shit yet\n");
     printf("\n");
     closedir(dr);
-	return (0);
+}
+
+unsigned char		get_flags(int ac, char **av)
+{
+	int				i;
+	char			**str;
+	unsigned char	flags;
+
+	flags = 0;
+	i = 1;
+	while (i < ac)
+	{
+		str = &av[i];
+		if (**str == '-')
+		{
+			(*str)++;
+			while (**str == 'l' || **str == 'a' || **str == 'R' ||
+			**str == 'r' || **str == 't')
+			{
+				if (**str == 'l')
+					flags |= 1;
+				if (**str == 'a')
+					flags |= 2;
+				if (**str == 'R')
+					flags |= 4;
+				if (**str == 'r')
+					flags |= 8;
+				if (**str == 't')
+					flags |= 16;
+				(*str)++;
+			}
+		}
+		i++;
+	}
+	return (flags);
+}
+
+int main(int ac, char **av)
+{
+		return (0);
 }
