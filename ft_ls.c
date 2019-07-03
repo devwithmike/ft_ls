5f80033f249056a24c56ebaed3cadef887fc3e55
Michael MEYER <mimeyer@c4r21s3.wethinkcode.co.za>
2019-07-03 09:51:05 +0200

    jdsfh

--- ft_ls.c
+++ ft_ls.c
@@ -6,22 +6,37 @@
 /*   By: mimeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 08:53:30 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/07/02 16:04:32 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/07/03 09:50:53 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "includes/ft_ls.h"
 
-unsigned char		get_flags(int ac, char **av)
+int				check_flags(char **str)
+{
+	if (**str == 'l')
+		return 1;
+	else if (**str == 'a')
+		return 2;
+	else if (**str == 'R')
+		return 4;
+	else if (**str == 'r')
+		return 8;
+	else if (**str == 't')
+		return 16;
+	return 0;
+}
+
+unsigned char	get_flags(int ac, char **av)
 {
 	int				i;
 	char			**str;
 	unsigned char	flags;
 
-	flags = 0;
-	i = 1;
-	while (i < ac)
+	flags = '\0';
+	i = 0;
+	while (++i < ac)
 	{
 		str = &av[i];
 		if (**str == '-')
@@ -30,43 +45,49 @@ unsigned char		get_flags(int ac, char **av)
 			while (**str == 'l' || **str == 'a' || **str == 'R' ||
 			**str == 'r' || **str == 't')
 			{
-				if (**str == 'l')
-					flags |= 1;
-				else if (**str == 'a')
-					flags |= 2;
-				else if (**str == 'R')
-					flags |= 4;
-				else if (**str == 'r')
-					flags |= 8;
-				else if (**str == 't')
-					flags |= 16;
+				flags |= check_flags(str);
 				(*str)++;
 			}
 		}
-		i++;
 	}
 	return (flags);
+}
+
+void	ft_ls(char *path, unsigned char flags)
+{
+	struct dirent *de;
+
+	DIR *dr = opendir(path);
+	while ((de = readdir(dr)) != NULL)
+		if (!flags)
+		{
+			if (de->d_name[0] != '.')
+			{
+				ft_putstr(de->d_name);
+				ft_putchar('\t');
+			}
+		}
+    	else
+		{
+    		ft_putstr(de->d_name);
+    		ft_putchar('\t');
+    	}
 }
 
 int main (int ac, char **av)
 {
 	unsigned char flags;
-	char **str = NULL;
 	int i;
 
-	i = 2;
+	i = 0;
 	flags = get_flags(ac, av);
-	while (i < ac)
-	{
-		str = &av[i];
-		if (**str == '-')
-			(*str)++;
-		else
+	if (ac == 1)
+		ft_ls(".", flags);
+	else
+		while (++i < ac)
 		{
-			printf("%s\n", *str);
-			(*str)++;
+			ft_ls(av[i],flags);
 		}
-		i++;
-	}
+	ft_putchar('\n');
 	return (0);
 }
