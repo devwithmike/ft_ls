9f355a05d704297d8b21957e2574da24c2d784de
Michael MEYER <mimeyer@c4r21s3.wethinkcode.co.za>
2019-07-10 13:43:28 +0200

    sjdhjdsh

--- srcs/printing.c
+++ srcs/printing.c
@@ -6,7 +6,7 @@
 /*   By: mimeyer <mimeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 14:48:22 by mimeyer           #+#    #+#             */
-/*   Updated: 2019/07/10 11:20:19 by mimeyer          ###   ########.fr       */
+/*   Updated: 2019/07/10 13:30:13 by mimeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,32 @@ void	print_blocks(t_dir *list)
 		list = list->next;
 	}
 	ft_putnbr(i);
+	ft_putchar('\n');
+}
+
+void	format_line(t_dir *list)
+{
+	ft_putstr((S_ISDIR(list->mode)) ? "d" : "-");
+	ft_putstr((list->mode & S_IRUSR) ? "r" : "-");
+	ft_putstr((list->mode & S_IWUSR) ? "w" : "-");
+	ft_putstr((list->mode & S_IXUSR) ? "x" : "-");
+	ft_putstr((list->mode & S_IRGRP) ? "r" : "-");
+	ft_putstr((list->mode & S_IWGRP) ? "w" : "-");
+	ft_putstr((list->mode & S_IXGRP) ? "x" : "-");
+	ft_putstr((list->mode & S_IROTH) ? "r" : "-");
+	ft_putstr((list->mode & S_IWOTH) ? "w" : "-");
+	ft_putstr((list->mode & S_IXOTH) ? "x  " : "-  ");
+	ft_putnbr(list->nlink);
+	ft_putchar('\t');
+	ft_putstr(list->uid);
+	ft_putstr("  ");
+	ft_putstr(list->gid);
+	ft_putstr("  ");
+	ft_putnbr(list->size);
+	ft_putstr("\t");
+	ft_putnbr(list->time);
+	ft_putstr(" ");
+	ft_putendl(list->name);
 }
 
 void	print_list(t_dir *list, unsigned char flags)
@@ -34,20 +60,13 @@ void	print_list(t_dir *list, unsigned char flags)
 	while (ptr != NULL)
 	{
 		if (flags & 2)
-		{
-			printf("%hu\t%s\t%s\t%lld\t%s\n", ptr->nlink, ptr->uid, ptr->gid,
-					ptr->size, ptr->name);
-			ptr = ptr->next;
-		}
+			format_line(ptr);
 		else
 		{
-			if (ptr->name[0] != '.')
-			{
-				printf("%hu\t%s\t%s\t%lld\t%s\n", ptr->nlink, ptr->uid,
-						ptr->gid, ptr->size, ptr->name);
-			}
-			ptr = ptr->next;
+			if (ft_strncmp(ptr->name, ".", 1) != 0)
+				format_line(ptr);
 		}
+		ptr = ptr->next;
 	}
 }
 
@@ -78,7 +97,7 @@ void	print_normal(t_dir *list, unsigned char flags)
 
 void	print_recursion(char *path)
 {
-	if ((ft_strcmp(path, "./") != 0) && (ft_strcmp(path, ".") != 0))
+	if ((ft_strcmp(path, "./") != 0) || (ft_strcmp(path, ".") != 0))
 	{
 		ft_putstr("\n");
 		ft_putstr(path);
